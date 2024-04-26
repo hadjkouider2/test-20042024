8c8faa616198eeb6e8baa75030ccba5045b82cfa
unknown <hadjkouiderbenboualihk@gmail.com>
2024-04-27 00:19:50 +0100

    add

--- JAVASCRIPT/javascript.js
+++ JAVASCRIPT/javascript.js
@@ -27,12 +27,13 @@ function gettotal()
     function CloneEle() {
        var x= document.getElementById("inputs");
        var clone = x.cloneNode(true);
+       
+    //    var removeButton = document.createElement('Button');
+    //    removeButton.innerHTML = '<ion-icon name="trash-outline" class="delete"></ion-icon>';
+    //    clone.appendChild(removeButton);
+    //    removeButton.setAttribute("class","remove");
        document.body.appendChild(clone);
 
-       var removeButton = document.createElement('Button');
-       removeButton.innerHTML = '<ion-icon name="trash-outline" class="delete"></ion-icon>';
-       clone.appendChild(removeButton);
-       removeButton.setAttribute("class","remove");
       
   clearData()
   
@@ -49,38 +50,36 @@ function clearData(){
 }
 
 
-
+$(document).ready(function(){
     
-    
-
-
-
-
-
-
-
-
-    // $(".remove").click(function(){
-    //     document.body.remove();
-    //     var x = $("#inputs").length
-    //      if (x = 2 ){
-    //       $(this).attr({
-    //         disabled:'disabled'
-    //       })
-    //   }else{
-       
-    //     }      
-
-    // }) 
-       
-    
+    var clone = x.cloneNode(true);
+    $(document).on('click','#remove',function(){
       
-     
+
+       console.log($(this).parent());
+       $(this).parent().remove()
+
+    })
+})    
+$(document).ready(function(){
+    $("#inputs").on('click','#remove',function(event){
+        event.preventDefault();
+        var rem =$(this).parent()
+        console.log(rem);
+       
+        var cpt = $("#inputs").length
+        if(cpt>1){
+             rem.remove()
+        }else{
+            $(this).attr("disabled","disabled");
+            $(this).css({background :'grey',
+        })
+        }
     
-// $(document).ready(function(){
-//     var clone = x.cloneNode(true);
-//     $('.remove').on('click','#delete',function(){
-//         document.body.remove();
-//     })
-// })    
+    
+    
+    
+    })
+
+})
 
